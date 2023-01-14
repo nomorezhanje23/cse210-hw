aaaa5107591042a80fdffe2aa98ffa646ccedc65
Nomore Zhanje <nomorezhanje100@gmail.com>
2023-01-15 00:50:24 +0200

    Added Last name and the statement

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,10 @@ class Program
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
-        Console.WriteLine(first);
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"My name is {first}, {first} {last}");
     
     }
 }
