4dc1df84cc34d0c0329789f8c2f8538d19416069
Nomore Zhanje <nomorezhanje100@gmail.com>
2023-01-26 15:55:38 +0200

    Homework Noted W4

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,8 +6,7 @@ class Program
     {
         DisplayWelcomeMessage();
 
-        string entry();
-        string prompt();
+      
 
         DisplayResultMessage();
 
@@ -21,44 +20,8 @@ class Program
     static void DisplayWelcomeMessage()
     {
         Console.WriteLine("Welcome to your journal, a place where memories live!");
-        Console.WriteLine ("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-
+       
     }
-
-    static string entry()
-    {
-        Console.Write("What would you like to do? ");
-        magicNumber = int.Parse(Console.ReadLine());
-
-        return magicNumber; 
-
-         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 6);
-
-        int prompt = -1;
-
-        while (prompt == magicNumber)
-
-        {
-              
-
-              if (prompt == 1)
-              {
-                Console.WriteLine("If I had one thing I could do today what would it be? ");
-              }
-              //else if (prompt == 2)
-
-
-    }
-
-
-        }
-
 
     static void DisplayResultMessage(int prompt)
     {
